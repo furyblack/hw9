a5dcaad210d72114bf00d0a0b07ac9826dc5d879
Mikhail Sergeev <sergeevmikhail.dev@gmail.com>
2024-07-13 09:12:56 +0500

    pass 11 tests from 20 now get active devises works well

--- src/repositories/query-session-repository.ts
+++ src/repositories/query-session-repository.ts
@@ -1,7 +1,24 @@
-import {SessionType} from "../types/session/sessionType";
+import {sessionCollection} from "../db/db";
 
 export class QuerySessionRepository {
-    // static async getActiveDevices(userId: string, deviceId: string): Promise<SessionType[]> {
-    //
-    // }
+    static async getActiveDevises(userId:string, liveTime: number){
+        const now = new Date()
+        const newTime = new Date(now.getTime()-liveTime*1000)
+        try {
+            const sessions = await sessionCollection.find({
+                userId:userId,
+                lastActiveDate:{ $gt: newTime}
+            }).toArray()
+            const formatedSessions = sessions.map(session=>({
+                ip:session.ip,
+                title: session.title,
+                lastActiveDate:session.lastActiveDate.toISOString(),
+                deviceId:session.deviceId
+            }))
+            return formatedSessions
+        }catch (error){
+            console.error('Error fetching active devices', error)
+            throw new Error('database query failed')
+        }
+    }
 }
--- src/routes/device-router.ts
+++ src/routes/device-router.ts
@@ -1,8 +1,7 @@
 import {Router, Response, Request} from "express";
-
 import {authMiddlewareRefresh} from "../middlewares/auth/auth-middleware";
-import {refreshTokenExpiration} from "../application/jwt-service";
-import {sessionCollection} from "../db/db";
+import {refreshTokenExpiration} from "../application/jwt-service"
+import {QuerySessionRepository} from "../repositories/query-session-repository";
 
 
 
@@ -13,14 +12,22 @@ deviceRouter.get('/devices', authMiddlewareRefresh, async (req: Request, res: Re
     const liveTime = refreshTokenExpiration //время жизни рефрештокена в секундах
     // Получаем текущую дату и время
     const now = new Date();
+   try {
+       const sessions = await QuerySessionRepository.getActiveDevises(userId, liveTime)
+       res.status(200).send(sessions)
+   }catch (error){
+       res.status(500).send({error:'something goes wrong'})
+   }
 
 // Вычисляем новое время, отнимая 20 секунд
-    const newTime = new Date(now.getTime() - liveTime * 1000);
-    const sessions = await sessionCollection.find(({
-        userId: userId,
-        lastActiveDate: { $gt: newTime }
-    })).toArray()
-    console.log(sessions)
-    res.status(200).send(sessions)
+//     const newTime = new Date(now.getTime() - liveTime * 1000);
+//     const sessions = await sessionCollection.find(({
+//         userId: userId,
+//         lastActiveDate: { $gt: newTime }
+//     })).toArray()
+//     res.status(200).send(sessions)
 })
 
+deviceRouter.delete('/devises', authMiddlewareRefresh, async (req:Request, res:Response)=>{
+
+})
