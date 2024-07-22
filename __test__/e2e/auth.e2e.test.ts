89929376c9f01e6dd42d75dc96235617883d647d
Mikhail Sergeev <sergeevmikhail.dev@gmail.com>
2024-07-22 13:25:31 +0500

    add tests for deleting sessions by deviceId and all sessions exept active session

--- __test__/e2e/auth.e2e.test.ts
+++ __test__/e2e/auth.e2e.test.ts
@@ -16,7 +16,8 @@ const userCreateData = {
 
 let user;
 let firstRefreshToken: any;
-let secondRefreshToken: any
+let thirdRefreshToken: any
+
 
 describe('/auth', () => {
     beforeAll(async () => {
@@ -38,7 +39,7 @@ describe('/auth', () => {
     it('should login user first time', async () => {
         const loginResponse = await request(app)
             .post('/auth/login')
-            .send({ loginOrEmail: userCreateData.login, password: userCreateData.password })
+            .send({loginOrEmail: userCreateData.login, password: userCreateData.password})
             .expect(200);
         firstRefreshToken = loginResponse.headers['set-cookie'][0].split(';')[0].split('=')[1];
 
@@ -53,7 +54,7 @@ describe('/auth', () => {
     it('should login user second time', async () => {
         const loginResponse = await request(app)
             .post('/auth/login')
-            .send({ loginOrEmail: userCreateData.login, password: userCreateData.password })
+            .send({loginOrEmail: userCreateData.login, password: userCreateData.password})
             .expect(200);
         const secondRefreshToken = loginResponse.headers['set-cookie'][0].split(';')[0].split('=')[1];
 
@@ -72,8 +73,78 @@ describe('/auth', () => {
             .expect(204);
 
     });
-});
 
+    //третий логин который будем удалять
+    it('should login user third time', async () => {
+        const loginResponse = await request(app)
+            .post('/auth/login')
+            .send({loginOrEmail: userCreateData.login, password: userCreateData.password})
+            .expect(200);
+        const thirdRefreshToken = loginResponse.headers['set-cookie'][0].split(';')[0].split('=')[1];
+
+        const sessionsResponse3 = await request(app)
+            .get('/security/devices')
+            .set('Cookie', `refreshToken=${thirdRefreshToken}`)
+            .expect(200);
+
+        expect(sessionsResponse3.body).toHaveLength(2);
+    });
+
+    it('should delete all other sessions', async () => {
+        // вход пользователя для получения активного токена сессии
+        const loginResponse = await request(app)
+            .post('/auth/login')
+            .send({loginOrEmail: userCreateData.login, password: userCreateData.password})
+            .expect(200);
+
+        const activeSessionToken = loginResponse.headers['set-cookie'][0].split(';')[0].split('=')[1];
+
+        // удаление всех других сессий, кроме текущей активной
+        await request(app)
+            .delete('/security/devices')
+            .set('Cookie', `refreshToken=${activeSessionToken}`)
+            .expect(204);
+    });
+    it('should delete sesssion by deviceId', async () => {
+        //логинимся и получаем активный токен сессии
+        const loginResponse = await request(app)
+            .post('/auth/login')
+            .send({loginOrEmail: userCreateData.login, password: userCreateData.password})
+            .expect(200);
+        const activeSessionToken = loginResponse.headers['set-cookie'][0].split(';')[0].split('=')[1];
+
+        // получаем список сессий чтобы извлечь deviceId
+        const sessionsResponse = await request(app)
+            .get('/security/devices')
+            .set('Cookie', `refreshToken=${activeSessionToken}`)
+            .expect(200);
+
+        //должна быть хотя бы 1 сессия
+        const deviceId = sessionsResponse.body[0].deviceId
+
+        //удаляем конкретную сессия по deviceId
+        await request(app)
+            .delete(`/security/devices/${deviceId}`)
+            .set('Cookie', `refreshToken=${activeSessionToken}`)
+            .expect(204);
+
+        //провермяем что сессия удалена
+        const sessionsResponseAfterDeletion = await request(app)
+            .get('/security/devices')
+            .set('Cookie', `refreshToken=${activeSessionToken}`)
+            .expect(200);
+
+        // Ожидаем, что в списке нет сессии с deviceId, которую мы удалили
+        interface Session {
+            deviceId: string;
+        }
+
+        const deletedSession = (sessionsResponseAfterDeletion.body as Session[]).find(
+            (session: Session) => session.deviceId === deviceId
+        );
+        expect(deletedSession).toBeUndefined();
+    });
+})
 //после разделения токена
 // 1 запросить сессии должна быть одна
 // 2) второй логин появляется вторая
